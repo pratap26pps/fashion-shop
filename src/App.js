ee6d7c069a19b79b9be2209dc26758137a62f7c0
pratap26pps <pankajpatna10321@gmail.com>
2024-10-07 05:02:18 +0530

    new line

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Cart from './component/Cart';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
+import { NavLink } from 'react-router-dom';
 function App() {
   return (
     <div className="text-center   ">
 
+   
+     
+          
      <div className='bg-lime-300 p-1 fixed w-full top-0'>
        <Navbar></Navbar>
      </div>
-     
+
      
       <Routes >
         <Route path="/home" element={<Home/>}></Route>
