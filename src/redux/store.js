710f4446ceca3bd48968a1e4fb39c5bb728cb56f
pankaj pratap singh <pankajpatna10321@gmail.com>
2024-10-07 07:29:33 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { Addtocart } from './slices/Addtocart'
 import CartReducer from './slices/Addtocart'
 export const store = configureStore({
   reducer: {
