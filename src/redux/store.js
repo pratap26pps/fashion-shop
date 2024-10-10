48d9cb73a25a5a20d524a46f656f6db204aa4c92
pratap26pps <pankajpatna10321@gmail.com>
2024-10-11 01:41:05 +0530

    no change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
-<<<<<<< HEAD
-
-=======
->>>>>>> 710f4446ceca3bd48968a1e4fb39c5bb728cb56f
-import CartReducer from './slices/Addtocart'
+import { configureStore } from '@reduxjs/toolkit';
+import CartReducer from './slices/Addtocart';
 export const store = configureStore({
   reducer: {
     cart:CartReducer,
   },
-})
+});
